2fb969b2dc9b873e324e254daf274a43334d9465
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-10-26 16:08:12 +0700

    Add project files.

---
+++ SearchAlgorithmm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+        {
+        }
+    }
+}
